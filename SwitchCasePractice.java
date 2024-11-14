978569d55f977ee64f5cf4e53ac37960a188852b
SMHaari <allinallsquad@gmail.com>
2024-11-14 21:19:13 +0530

    Switch case code

---
+++ SwitchCasePractice.java
@@ -0,0 +1,47 @@
+package hello_java;
+import java.util.Scanner;
+class CustomException extends Exception{
+	CustomException(String name){
+		super(name);
+	}
+}
+public class SwitchCasePractice {
+	//@SuppressWarnings("resource")
+	public static void main(String[]args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the day number : ");
+		try {
+			int Day=sc.nextInt();
+			if (Day>7) {
+				throw new CustomException("A week has only 7 days, Enter the valid day number");
+			}
+		switch(Day) {
+			case 1:
+				System.out.println("Sunday");
+				break;
+			case 2:
+				System.out.println("Monday");
+				break;
+			case 3:
+				System.out.println("Tuesday");
+				break;
+			case 4:
+				System.out.println("Wednesday");
+				break;
+			case 5:
+				System.out.println("Thursday");
+				break;
+			case 6:
+				System.out.println("Friday");
+				break;
+			case 7:
+				System.out.println("Saturday");
+		}
+		sc.close();
+	}catch(CustomException e) {
+		System.out.println(e.getMessage());
+	}
+		
+	}
+
+}
